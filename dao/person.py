dde9fc6b3bce5d275ea48effe36a2a5bb58f412a
luisvega5 <luis.vega5@upr.edu>
2018-03-24 18:58:38 -0400

    Post and Get Operations of Person

--- dao/person.py
+++ dao/person.py
@@ -3,11 +3,15 @@ class PersonDAO:
         P1 = [1, 'Luis','Vega', '787-634-1091', 'luis.vega5@upr.edu']
         P2 = [5, 'Eric', 'Santillana', '939-089-1011', 'eric.santillana@upr.edu']
         P3 = [10, 'Fernando', 'Ortiz', '122-059-9031', 'fernando.ortiz@upr.edu']
+        P4 = [1, 'Petraca', 'Rivera', '787-990-293', 'mamichulitaexotica@gmail.edu']
+        P5 = [5, 'Indy', 'Flow', '787-010-1111', 'laduraquita@hotmail.edu']
 
         self.data = []
         self.data.append(P1)
         self.data.append(P2)
         self.data.append(P3)
+        self.data.append(P4)
+        self.data.append(P5)
 
     def getAllPersons(self):
         return self.data
@@ -17,3 +21,12 @@ class PersonDAO:
             if id == r[0]:
                 return r
         return None
+
+    def insert(self):
+        # cursor = self.conn.cursor()
+        # query = "insert into Person(pFirstName, pLastName, pPhone, pEmail) values (%s, %s, %s, %s) returning pid;"
+        # cursor.execute(query, (pFirstName, pLastName, pPhone, pEmail))
+        # pid = cursor.fetchone()[0]
+        # self.conn.commit()
+        pid = 30
+        return pid
