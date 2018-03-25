98186142afacf4d1193b91d203b63d457e629067
luisvega5 <luis.vega5@upr.edu>
2018-03-24 22:16:36 -0400

    Post and Get Operations of Person

--- handler/person.py
+++ handler/person.py
@@ -10,6 +10,15 @@ class PersonHandler:
         result['pEmail'] = row[4]
         return result
 
+    def build_person_attributes(self, pID, pFirstName, pLastName, pPhone, pEmail):
+        result = {}
+        result['pID'] = pID
+        result['pFirstName'] = pFirstName
+        result['pLastName'] = pLastName
+        result['pPhone'] = pPhone
+        result['pEmail'] = pEmail
+        return result
+
     def getAllPersons(self):
         dao = PersonDAO()
         result = dao.getAllPersons()
@@ -28,6 +37,7 @@ class PersonHandler:
             return jsonify(Person=mapped)
 
     def insertPerson(self,form):
+        print("Hola"+form[0]+"\n")
         if len(form) != 5:
             return jsonify(Error="Malformed post request"), 400
         else:
@@ -37,13 +47,10 @@ class PersonHandler:
             pEmail = form['pEmail']
             if pFirstName and pLastName and pPhone and pEmail:
                 dao = PersonDAO()
-                pid = dao.insert(pFirstName, pLastName, pPhone, pEmail)
+                pID = dao.insert(pFirstName, pLastName, pPhone, pEmail)
                 result = {}
-                result['pID','pFirstName','pLastName','pPhone','pEmail']
+                result = self.build_person_attributes(pID, pFirstName, pLastName, pPhone, pEmail)
                 return jsonify(Person=result), 201
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
-        dao = PersonDAO()
-        result = dao.createNewPerson(data)
-        return jsonify(Person=result)
 
